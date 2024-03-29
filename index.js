e90ea1044c403f9df4d5dd7be61784717d7075de
Victoria Sternau <victoria.sternau@student.swic.edu>
2024-03-28 21:03:16 -0500

    Create loop for function verifyAdulthood

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const person = {
-  userName: "John",
-  userAge: 17,
-};
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
 
 function verifyAdulthood(personName, personAge) {
   if (personAge >= 18) return `Welcome, ${personName}!`;
@@ -9,6 +27,8 @@ function verifyAdulthood(personName, personAge) {
   return `You are not old enough to enter, ${personName}.`;
 }
 
-const adultMessage = verifyAdulthood(person.userName, person.userAge);
-
-console.log(adultMessage);
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  const result = verifyAdulthood(person.name, person.age);
+  console.log(result);
+}
